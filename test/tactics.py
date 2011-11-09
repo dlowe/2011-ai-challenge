c539f0ff57176f55589feb631a3c2f19a94d339c
J. David Lowe <j.david.lowe@gmail.com>
2011-11-08 18:54:27 -0800

    tactical tests representing the problems to solve

--- test/tactics.py
+++ test/tactics.py
@@ -53,5 +53,46 @@ class TestGreed(TestTactic, unittest.TestCase):
         ## food is reachable in 3 moves
         self.assertFoodEaten(1, 1, turn=3)
 
+class TestDoubleGreed(TestTactic, unittest.TestCase):
+    """In the absence of other inputs, ants should gather food efficiently, even if they have
+       to move in opposite directions!"""
+
+    map = """
+          %%%%%%%%%%%%%%%%%%%%
+          %*..A.a.....*%%%%%B%
+          %%%%%%%%%%%%%%%%%%%%
+          """
+
+    def assertions(self, game):
+        ## no dying!
+        self.assertSurvived()
+        self.assertEquals(game['replaydata']['cutoff'], 'turn limit reached')
+
+        ## food are reachable in 2 and 3 moves, respectively
+        self.assertFoodEaten(1, 1, turn=2)
+        self.assertFoodEaten(1, 12, turn=5)
+
+class TestShitloadsOfFoodAndAnts(TestTactic, unittest.TestCase):
+    """Ants should be able to gather food efficiently even when there's lots of it and lots of them."""
+
+    repeat = 200
+    map = ('%%%%' * repeat + '\n'
+         + '%*.A' * repeat + '\n'
+         + '%%%%' * repeat + '\n'
+         + '%%%%' * repeat + '\n'
+         + '%%%%' * repeat + '\n'
+         + '%%%%' * repeat + '\n'
+         + '%%%%' * repeat + '\n'
+         + '%%B%' * repeat + '\n'
+         + '%%%%' * repeat + '\n')
+
+    def assertions(self, game):
+        ## no dying!
+        self.assertSurvived()
+        self.assertEquals(game['replaydata']['cutoff'], 'turn limit reached')
+
+        for x in range(self.repeat):
+            self.assertFoodEaten(1, x * 4 + 1, turn=1)
+
 if __name__ == '__main__':
     unittest.main(argv=sys.argv)
